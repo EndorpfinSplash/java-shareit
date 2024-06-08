331f82f4ceb6f5293d794c40789f227025d0a02f
Andrey Zinovich <fast.optimus@gmail.com>
2024-06-08 08:46:56 +0300

    style: comment inMemImpl to reduce test coverage

--- src/main/java/ru/practicum/shareit/item/dao/ItemStorage.java
+++ src/main/java/ru/practicum/shareit/item/dao/ItemStorage.java
@@ -1,18 +1,18 @@
-package ru.practicum.shareit.item.dao;
-
-import ru.practicum.shareit.item.model.Item;
-
-import java.util.Collection;
-import java.util.Optional;
-
-public interface ItemStorage {
-    Item save(Item item);
-
-    Optional<Item> updateItem(Integer itemId, Item item);
-
-    Optional<Item> findById(Integer itemId);
-
-    Collection<Item> findByOwner(Integer userId);
-
-    Collection<Item> findByNameOrDescription(String text);
-}
+//package ru.practicum.shareit.item.dao;
+//
+//import ru.practicum.shareit.item.model.Item;
+//
+//import java.util.Collection;
+//import java.util.Optional;
+//
+//public interface ItemStorage {
+//    Item save(Item item);
+//
+//    Optional<Item> updateItem(Integer itemId, Item item);
+//
+//    Optional<Item> findById(Integer itemId);
+//
+//    Collection<Item> findByOwner(Integer userId);
+//
+//    Collection<Item> findByNameOrDescription(String text);
+//}
--- src/main/java/ru/practicum/shareit/user/dao/UserStorage.java
+++ src/main/java/ru/practicum/shareit/user/dao/UserStorage.java
@@ -1,16 +1,16 @@
-package ru.practicum.shareit.user.dao;
-
-import ru.practicum.shareit.user.User;
-
-import java.util.Collection;
-import java.util.Optional;
-
-public interface UserStorage {
-    Collection<User> findAll();
-
-    User save(User user);
-
-    Optional<User> findById(Integer id);
-
-    void deleteById(Integer id);
-}
+//package ru.practicum.shareit.user.dao;
+//
+//import ru.practicum.shareit.user.User;
+//
+//import java.util.Collection;
+//import java.util.Optional;
+//
+//public interface UserStorage {
+//    Collection<User> findAll();
+//
+//    User save(User user);
+//
+//    Optional<User> findById(Integer id);
+//
+//    void deleteById(Integer id);
+//}
