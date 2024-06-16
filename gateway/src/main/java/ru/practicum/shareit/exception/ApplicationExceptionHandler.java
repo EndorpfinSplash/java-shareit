e21a324a683405847da0639d29bc20763531116e
Andrey Zinovich <fast.optimus@gmail.com>
2024-06-16 16:03:52 +0300

    perf: catch body validation exception

--- gateway/src/main/java/ru/practicum/shareit/exception/ApplicationExceptionHandler.java
+++ gateway/src/main/java/ru/practicum/shareit/exception/ApplicationExceptionHandler.java
@@ -2,6 +2,7 @@ package ru.practicum.shareit.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -19,6 +20,16 @@ public class ApplicationExceptionHandler {
     public ErrorResponse catchValidation(final RuntimeException e) {
         return new ErrorResponse(e.getMessage(),
                 "Incorrect booking state was send.");
+    }
+
+
+    @ExceptionHandler({
+            MethodArgumentNotValidException.class,
+    })
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse catchBodyValuesValidation(final MethodArgumentNotValidException e) {
+        return new ErrorResponse(e.getMessage(),
+                "Incorrect field was send in the body.");
     }
 
 }
